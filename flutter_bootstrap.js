abdc81cc6009851c86a440f6d8e4acfde214e9d0
thantzin <dev.thantzin@gmail.com>
2025-08-21 21:44:55 +0700

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2100445010"
+    serviceWorkerVersion: "4219802404"
   }
 });
