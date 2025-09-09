df8c816f03ad51a38fcd50f49453b43af6ac3c56
thantzin <dev.thantzin@gmail.com>
2025-09-09 16:45:42 +0700

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c0b8f11e74a9cfc0ddf0810132ee3956",
+const RESOURCES = {"flutter_bootstrap.js": "64b3aebb4c32b6e9de01ae628c9ed21f",
 "version.json": "009c9e65172e010890f7f65fde438006",
 "index.html": "5b1aa18857128c4bdf1934fe3a922ce8",
 "/": "5b1aa18857128c4bdf1934fe3a922ce8",
-"main.dart.js": "144e5bf7ba5d6dc7e95d61b44c2cb2c4",
+"main.dart.js": "17dec9b40c6dc37d8da10e7c6a365be6",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "README.md": "fefe63f22c078f3ee3fb93a1486f978e",
 "favicon.png": "a2a33432919cd920280d243b7db1d90c",
